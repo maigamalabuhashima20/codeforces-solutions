0c0e8dcbc9707782c8bba486eab7234523e1c3f4
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:52:42 +0200

    Committing files

---
+++ CodeForces/GNU C++17/231A | Team/145532699.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n;
+ short p1, p2, p3, result = 0;
+ cin >> n;
+ for (int i = 0; i < n; i++)
+ {
+  short sum = 0;
+  cin >> p1 >> p2 >> p3;
+  sum = p1 + p2 + p3;
+  if (sum >= 2)
+   result += 1;
+ }
+cout << result << endl;
+return 0;
+}
