22710b09ef5c151117d7a7d52c28292e91e18c31
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:49:38 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1722A | Spell Check/170788227.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string sortString(string &str)
+{
+    sort(str.begin(), str.end());
+    return str;
+}
+int main()
+{
+    short t, n;
+    string s, temp = "Timru";
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cin >> s;
+        if (n == 5)
+        {
+            if (sortString(s) == temp)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+ 
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
