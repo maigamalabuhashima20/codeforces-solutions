4ac416bd08b74b766a5cc5d43b8193d52dcc1a5a
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:53:28 +0200

    Committing files

---
+++ CodeForces/GNU C++17/50A | Domino piling/117418988.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m , n , multiply , domino ;
+    cin >> m>> n;
+    multiply = m * n;
+    domino = 2 * 1;
+    cout << multiply / domino <<endl;
+    
+    return 0;
+}
