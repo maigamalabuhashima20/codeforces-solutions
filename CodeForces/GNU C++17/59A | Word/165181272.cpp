607eb538cc0d8bb83c90548fa03cd8645430543e
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:50:24 +0200

    Committing files

---
+++ CodeForces/GNU C++17/59A | Word/165181272.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    short u = 0 , l = 0;
+    for(int i = 0; i < s.length() ; i++)
+    {
+        if (isupper(s[i]))
+            u++;
+        else
+            l++;
+    }
+    if (u > l)
+    {
+        for(int i = 0; i < s.length() ; i++)
+            s[i] = toupper(s[i]);
+        cout<<s;
+    }
+    else
+    {
+    for(int i = 0; i < s.length() ; i++)
+        s[i] = tolower(s[i]);
+    cout<<s;
+    }
+    
+    return 0;
+}
