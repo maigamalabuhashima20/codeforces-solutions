902d7bf931518e049bcd26f615fbb3169bb65fc1
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:51:57 +0200

    Committing files

---
+++ CodeForces/GNU C++17/431A | Black Square/146569993.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+ 
+int main() {
+int arr[4];
+int result=0;
+for(int i=0;i<4;i++){
+ cin>>arr[i];
+}
+ 
+string s;
+cin>>s;
+int len=s.length();
+for(int i=0;i<len;i++){
+ int z=s[i]-'0';
+ //cout<<z;
+ result+=arr[z-1];
+ 
+}
+cout<<result;
+ 
+ 
+ return 0;
+}
