6c4e11d8f876fd6feaf7bebc00682d1798791e88
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:52:58 +0200

    Committing files

---
+++ CodeForces/GNU C++17/287A | IQ Test/127406205.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[4][5];
+    int counter, i, j;
+    for(i = 0; i < 4; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+           cin>>s[i][j];
+        }
+    }
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            counter = (s[i][j] == '#'? 1 : 0) + (s[i + 1][j] == '#'? 1 : 0) + (s[i][j + 1] == '#'? 1 : 0) + (s[i + 1][j + 1] == '#'? 1 : 0);
+            if(counter != 2)
+            {
+                cout<<"YES"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"NO"<<endl;
+ 
+ 
+    return 0;
+}
