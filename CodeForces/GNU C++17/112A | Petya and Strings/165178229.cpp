05ee2195810a2f89f16c81e57ce19198ff59c9c2
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:50:30 +0200

    Committing files

---
+++ CodeForces/GNU C++17/112A | Petya and Strings/165178229.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string fn , sn;
+    cin>>fn>>sn;
+    for(int i = 0; i < fn.size(); i++)
+    {
+        fn[i] = toupper(fn[i]);
+        sn[i] = toupper(sn[i]);
+    }
+    if(fn > sn)
+        cout<<1;
+    else if(fn < sn)
+        cout<<-1;
+    else
+        cout<<0;
+    return 0;
+}
