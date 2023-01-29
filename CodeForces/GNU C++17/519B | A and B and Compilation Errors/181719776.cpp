7ae944ffa135f92b155aab52951b9152d670cf68
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:48:34 +0200

    Committing files

---
+++ CodeForces/GNU C++17/519B | A and B and Compilation Errors/181719776.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#define improve                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0), cout.tie(0);
+ 
+using namespace std;
+int main()
+{
+    improve;
+ 
+    int n, t, sum1 = 0, sum2 = 0, sum3 = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t;
+        sum1 += t;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> t;
+        sum2 += t;
+    }
+    cout << sum1 - sum2 << endl;
+    for (int i = 0; i < n - 2; i++)
+    {
+        cin >> t;
+        sum3 += t;
+    }
+    cout << sum2 - sum3 << endl;
+ 
+    return 0;
+}
