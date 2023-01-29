2f3356bc3bc115bbcf58289b30a95e749ae777c8
Mai Gamal Abuhashima <76907152+maigamalabuhashima20@users.noreply.github.com>
2023-01-29 20:49:59 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1519B | The Cake Is a Lie/170774211.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short t, n, m;
+    int k;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> m >> k;
+        if ((n * m - 1) == k)
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+    return 0;
+}
